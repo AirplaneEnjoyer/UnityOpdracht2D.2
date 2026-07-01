9afab631979fdd0ac84d737b52f1eb91d2239a3a
AirplaneEnjoyer <2239378@talnet.nl>
2026-07-01 10:30:26 +0200

    Update Portal.cs

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -9,6 +9,15 @@ public class Portal : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
 
 
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private string _playerTag = "Player";
+
+
+
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(_playerTag))
         {
             SceneManager.LoadScene("ExitGame");
         }
